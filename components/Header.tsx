dbfdb05cd9c995f877acc61ae1b42f1167e51fd6
emanuel <emanuelgigena79@gmail.com>
2023-06-16 10:41:30 -0300

    fix commit with improvements in favicon and cleaning code

--- components/Header.tsx
+++ components/Header.tsx
@@ -16,21 +16,11 @@ export default function Header() {
                 className="h-full w-full object-contain"
                 src={img}
                 placeholder="blur"
-                alt="Cabañas del Sol"
+                alt="WebSite"
                 sizes="(max-width: 768px) 100vw, 50vw"
               />
             </Link>
             <div className="absolute inset-0 mix-blend-multiply" />
-          </div>
-          <div className="relative px-4 py-16 sm:px-6 sm:py-24 lg:py-32 lg:px-8">
-            {/* <h1 className="mt-1 text-center font-bold uppercase text-gray-900 sm:text-5xl sm:tracking-tight lg:text-7xl">
-              {/* <span className="block text-orange-500">Cabañas</span>  </h1> */}
-
-            {/* <div className="mx-auto mt-10 max-w-xs sm:flex sm:max-w-none sm:justify-center">
-              <button className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-orange-600 shadow-sm hover:bg-orange-100 sm:px-8">
-                Ver Mas
-              </button>
-            </div> */}
           </div>
         </div>
       </div>
