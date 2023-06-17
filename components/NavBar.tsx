ca37756576bdda42648c061f42b50892390bfbc6
emanuel <emanuelgigena79@gmail.com>
2023-06-17 14:08:41 -0300

    fix locations

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,20 +1,9 @@
 import React, { useState } from "react";
-import { Menu, Icon, Label } from "semantic-ui-react";
-import Cart from "./cart/Cart";
 import Link from "next/link";
 import logo from "../public/logo3.png";
 import Image from "next/image";
 
 const NavBar = () => {
-  const [isCartOpen, setIsCartOpen] = useState(false);
-
-  const handleCartOpen = () => {
-    setIsCartOpen(true);
-  };
-
-  const handleCartClose = () => {
-    setIsCartOpen(false);
-  };
 
   return (
     <nav className="bg-gray-900">
